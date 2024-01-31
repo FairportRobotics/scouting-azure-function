9f8c4c8ecf3703147372227b33eae98f3a800403
Michael Silva <mike.a.silva@gmail.com>
2024-01-31 10:59:37 -0500

    Merge from Dev (#2)
    * Add or update the Azure App Service build and deployment workflow config
    
    * Added in blob storage connection
    
    * Added JSON data parsing
    
    * Updated README
    
    * blob storage functionality added
    
    * Trying to debug
    
    * Reinstating local save of data
    
    * Trying to save data using pandas
    
    * Trying to save to /tmp
    
    * Now trying to save to blob storage
    
    * Cleaned up the code
    
    * Changed blob storage location
    
    * Rolled back changes
    
    * Changed blob storage location
    
    * Trying to save raw JSON
    
    * Bugfix?
    
    * Removed pandas stupid id form csv
    
    * Trying to save raw data again
    
    * Overwrite blob storage files on save
    
    * Added new data to csv
    
    * Trying to get cosmos working
    
    * Trying to connect to cosmos
    
    * Bugfix?
    
    * Bugfix?
    
    * Removed cosmos for now
    
    * Changed to use key vault
    
    * Rollback
    
    * Using key vault secrets
    
    * Trying to find the bug
    
    * Bug tracking
    
    * Added in default Azure Credential
    
    * Connecting to secrets vault
    
    * Moved blob storage connection code
    
    * Code cleanup
    
    * Test use of secrets
    
    * Try storage again
    
    * Checking secrets
    
    * Change type
    
    * initial commit
    
    * Trying the blob service client
    
    * Read existing data
    
    * Trying environment vars
    
    * Try again
    
    * Test
    
    * KISS
    
    * Deleted json
    
    * Take 2
    
    * Back to scratch
    
    * Add or update the Azure App Service build and deployment workflow config
    
    * Minor change
    
    * Parsing data
    
    * Saved JSON locally
    
    * Premature optimization
    
    * Match data bug fixed
    
    * Stupid me!
    
    * Trying to connect to the key vault
    
    * Updated GitHub Actions
    
    * Removed key vault
    
    * Trying the environmental variable approach
    
    * Trying to save to blob storage
    
    * Going all in

--- function_app.py
+++ function_app.py
@@ -1,25 +1,98 @@
 import azure.functions as func
+from azure.storage.blob import BlobServiceClient
 import logging
+import json
+import pandas as pd
+#from azure.keyvault.secrets import SecretClient
+#from azure.identity import DefaultAzureCredential
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-@app.route(route="v1")
-def v1(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
+def _get(req, key):
+    data = req.params.get(key)
+    if not data:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            data = req_body.get(key)
+    return data
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+
+@app.route(route="v1")
+def v1(req: func.HttpRequest) -> func.HttpResponse:
+    
+    logging.info('Parsing data.')
+    # Read in the data
+    data = _get(req, 'data')
+    data_type = _get(req, 'type')
+
+    if not data:
+        # Return a "helpful" message
+        return func.HttpResponse("Bummer!  No data sent to this endpoint.", status_code=200)
+    if not data_type:
+        # Return a "helpful" message
+        return func.HttpResponse("Bummer!  No type sent to this endpoint.", status_code=200)
+    
+    if data_type == "match":
+        message = handle_match_data(data)
+    return func.HttpResponse(message)
+
+
+def handle_match_data(data):
+    # Read the JSON data
+    j = json.loads(data)
+    # Flatten the JSON data
+    logging.info('Flattening JSON data.')
+    match_data = {}
+    for key,value in j.items():
+        if isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                match_data[key + "_" + sub_key] = sub_value
+        else:
+            match_data[key] = value
+    
+    logging.info('Connecting to blob storage.')
+    connection_string = os.environ["BLOB_STORAGE_CONNECTION_STRING"]
+    blob_service_client = BlobServiceClient.from_connection_string(conn_str=connection_string)
+    container_name = "crescendo"
+    
+    # Read in the existing data
+    logging.info('Read existing data.')
+    container_client = blob_service_client.get_container_client(container= container_name) 
+    with open(file="/tmp/existing.csv", mode="wb") as download_file:
+        download_file.write(container_client.download_blob("crescendo.csv").readall())
+    existing_df = pd.read_csv("/tmp/existing.csv")
+    # Drop any existing data with the same key
+    existing_df = existing_df[existing_df["key"] != match_data["key"]]
+    
+    # Save the raw JSON data
+    logging.info('Saving raw data locally.')
+    raw_path = "/tmp/" + match_data["key"]+".json"
+    with open(raw_path, "w") as f:
+        f.write(data)
+    
+    # Save the data locally
+    logging.info('Saving locally.')
+    df = pd.DataFrame([match_data])
+    df = pd.concat([existing_df, df])
+    local_file_name = f"/tmp/{container_name}.csv"
+    df.to_csv(local_file_name, index=False)
+        
+    # Transfer the local file to blob storage
+    logging.info('Saving to blob storage.')
+    # Create a blob client using the local file name as the name for the blob
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{container_name}.csv")
+    with open(file=local_file_name, mode="rb") as blob_data:
+        blob_client.upload_blob(blob_data, overwrite=True)
+        
+    # Save the raw JSON to blob storage
+    blob_client = blob_service_client.get_blob_client(container="raw", blob=match_data["key"]+".json")
+    with open(file=raw_path, mode="rb") as blob_data:
+        blob_client.upload_blob(blob_data, overwrite=True)
+    
+    # Indicate our successful save
+    return "Data synced to the cloud!"
+        
