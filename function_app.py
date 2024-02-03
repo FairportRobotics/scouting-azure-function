2f3337c99774800c4124219c51d1f481c31a972d
mikeasilva <mike.a.silva@gmail.com>
2024-02-03 11:16:38 -0500

    Black formated

--- function_app.py
+++ function_app.py
@@ -8,6 +8,7 @@ import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+
 def _get(req, key):
     data = req.params.get(key)
     if not data:
@@ -25,85 +26,105 @@ def v1(req: func.HttpRequest) -> func.HttpResponse:
     # Assumes the containers have been created
     container_name = "crescendo"
 
-    logging.info('Parsing data.')
+    logging.info("Parsing data.")
     # Read in the data
-    data = _get(req, 'data')
-    data_type = _get(req, 'type')
+    data = _get(req, "data")
+    data_type = _get(req, "type")
 
     if not data_type:
         # Return a "helpful" message
-        return func.HttpResponse(json.dumps({"message": "Bummer!  No type sent to this endpoint."}), mimetype="application/json", status_code=200)
-    
+        return func.HttpResponse(
+            json.dumps({"message": "Bummer!  No type sent to this endpoint."}),
+            mimetype="application/json",
+            status_code=200,
+        )
+
     if data_type in ["match", "pit"]:
         if not data:
             # Return a "helpful" message
-            return func.HttpResponse(json.dumps({"message": "Bummer!  No data sent to this endpoint."}), mimetype="application/json", status_code=200)
-    
+            return func.HttpResponse(
+                json.dumps({"message": "Bummer!  No data sent to this endpoint."}),
+                mimetype="application/json",
+                status_code=200,
+            )
+
     if data_type.lower() == "match":
         return_data = handle_match_data(data, container_name)
     elif data_type.lower() == "pit":
         return_data = handle_pit_data(data, container_name)
     else:
         return_data = {"message": "Unknown Data Type"}
-    return func.HttpResponse(json.dumps(return_data), mimetype="application/json", status_code=200)
-    
-        
+    return func.HttpResponse(
+        json.dumps(return_data), mimetype="application/json", status_code=200
+    )
 
 
 def handle_match_data(data, container_name):
     # Read the JSON data
     j = json.loads(data)
     # Flatten the JSON data
-    logging.info('Flattening JSON data.')
+    logging.info("Flattening JSON data.")
     match_data = {}
-    for key,value in j.items():
+    for key, value in j.items():
         if isinstance(value, dict):
             for sub_key, sub_value in value.items():
                 match_data[key + "_" + sub_key] = sub_value
         else:
             match_data[key] = value
 
-    logging.info('Connecting to blob storage.')
+    logging.info("Connecting to blob storage.")
     blob_connection_string = os.environ["BLOB_STORAGE_CONNECTION_STRING"]
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=blob_connection_string)
-    
+    blob_service_client = BlobServiceClient.from_connection_string(
+        conn_str=blob_connection_string
+    )
+
     # Read in the existing data
-    logging.info('Read existing data.')
-    container_client = blob_service_client.get_container_client(container=container_name) 
+    logging.info("Read existing data.")
+    container_client = blob_service_client.get_container_client(
+        container=container_name
+    )
     with open(file="/tmp/existing.csv", mode="wb") as download_file:
-        download_file.write(container_client.download_blob(f"{container_name}.csv").readall())
+        download_file.write(
+            container_client.download_blob(f"{container_name}.csv").readall()
+        )
     existing_df = pd.read_csv("/tmp/existing.csv")
     # Drop any existing data with the same key
     existing_df = existing_df[existing_df["key"] != match_data["key"]]
-    
+
     # Save the raw JSON data
-    logging.info('Saving raw data locally.')
-    raw_path = "/tmp/" + match_data["key"]+".json"
+    logging.info("Saving raw data locally.")
+    raw_path = "/tmp/" + match_data["key"] + ".json"
     with open(raw_path, "w") as f:
         f.write(data)
-    
+
     # Save the data locally
-    logging.info('Saving locally.')
+    logging.info("Saving locally.")
     df = pd.DataFrame([match_data])
     df = pd.concat([existing_df, df])
     local_file_name = f"/tmp/{container_name}.csv"
     df.to_csv(local_file_name, index=False)
-        
+
     # Transfer the local file to blob storage
-    logging.info('Saving to blob storage.')
+    logging.info("Saving to blob storage.")
     # Create a blob client using the local file name as the name for the blob
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{container_name}.csv")
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=f"{container_name}.csv"
+    )
     with open(file=local_file_name, mode="rb") as blob_data:
         blob_client.upload_blob(blob_data, overwrite=True)
-        
+
     # Save the raw JSON to blob storage
-    blob_client = blob_service_client.get_blob_client(container="raw", blob=match_data["key"]+".json")
+    blob_client = blob_service_client.get_blob_client(
+        container="raw", blob=match_data["key"] + ".json"
+    )
     with open(file=raw_path, mode="rb") as blob_data:
         blob_client.upload_blob(blob_data, overwrite=True)
-    
+
     # Save to Cosmos db
-    logging.info('Insert into Cosmos db.')
-    cosmos_client = CosmosClient(os.environ["COSMOS_URI"], credential=os.environ["COSMOS_KEY"])
+    logging.info("Insert into Cosmos db.")
+    cosmos_client = CosmosClient(
+        os.environ["COSMOS_URI"], credential=os.environ["COSMOS_KEY"]
+    )
     cosmos_database = cosmos_client.get_database_client(database=container_name)
     container = cosmos_database.get_container_client("match")
     # Add an id to the dictionary
@@ -113,7 +134,8 @@ def handle_match_data(data, container_name):
 
     # Indicate our successful save
     return {"message": "Data synced to the cloud!"}
-  
+
+
 def _get(req, key):
     data = req.params.get(key)
     if not data:
@@ -131,18 +153,22 @@ def v1(req: func.HttpRequest) -> func.HttpResponse:
     # Assumes the containers have been created
     container_name = "crescendo"
 
-    logging.info('Parsing data.')
+    logging.info("Parsing data.")
     # Read in the data
-    data = _get(req, 'data')
-    data_type = _get(req, 'type')
+    data = _get(req, "data")
+    data_type = _get(req, "type")
 
     if not data:
         # Return a "helpful" message
-        return func.HttpResponse("Bummer!  No data sent to this endpoint.", status_code=200)
+        return func.HttpResponse(
+            "Bummer!  No data sent to this endpoint.", status_code=200
+        )
     if not data_type:
         # Return a "helpful" message
-        return func.HttpResponse("Bummer!  No type sent to this endpoint.", status_code=200)
-    
+        return func.HttpResponse(
+            "Bummer!  No type sent to this endpoint.", status_code=200
+        )
+
     if data_type == "match":
         message = handle_match_data(data, container_name)
     return func.HttpResponse(message)
@@ -152,47 +178,59 @@ def handle_pit_data(data, container_name):
     # Read the JSON data
     pit_data = json.loads(data)
 
-    logging.info('Connecting to blob storage.')
+    logging.info("Connecting to blob storage.")
     blob_connection_string = os.environ["BLOB_STORAGE_CONNECTION_STRING"]
-    blob_service_client = BlobServiceClient.from_connection_string(conn_str=blob_connection_string)
-    
+    blob_service_client = BlobServiceClient.from_connection_string(
+        conn_str=blob_connection_string
+    )
+
     # Read in the existing data
-    logging.info('Read existing data.')
-    container_client = blob_service_client.get_container_client(container=container_name) 
+    logging.info("Read existing data.")
+    container_client = blob_service_client.get_container_client(
+        container=container_name
+    )
     with open(file="/tmp/existing.csv", mode="wb") as download_file:
-        download_file.write(container_client.download_blob(f"{container_name}_pit.csv").readall())
+        download_file.write(
+            container_client.download_blob(f"{container_name}_pit.csv").readall()
+        )
     existing_df = pd.read_csv("/tmp/existing.csv")
     # Drop any existing data with the same key
     existing_df = existing_df[existing_df["key"] != pit_data["key"]]
-    
+
     # Save the raw JSON data
-    logging.info('Saving raw data locally.')
-    raw_path = "/tmp/pit_" + pit_data["key"]+".json"
+    logging.info("Saving raw data locally.")
+    raw_path = "/tmp/pit_" + pit_data["key"] + ".json"
     with open(raw_path, "w") as f:
         f.write(data)
-    
+
     # Save the data locally
-    logging.info('Saving locally.')
+    logging.info("Saving locally.")
     df = pd.DataFrame([pit_data])
     df = pd.concat([existing_df, df])
     local_file_name = f"/tmp/{container_name}_pit.csv"
     df.to_csv(local_file_name, index=False)
-        
+
     # Transfer the local file to blob storage
-    logging.info('Saving to blob storage.')
+    logging.info("Saving to blob storage.")
     # Create a blob client using the local file name as the name for the blob
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=f"{container_name}_pit.csv")
+    blob_client = blob_service_client.get_blob_client(
+        container=container_name, blob=f"{container_name}_pit.csv"
+    )
     with open(file=local_file_name, mode="rb") as blob_data:
         blob_client.upload_blob(blob_data, overwrite=True)
-        
+
     # Save the raw JSON to blob storage
-    blob_client = blob_service_client.get_blob_client(container="raw", blob="pit_" + pit_data["key"]+".json")
+    blob_client = blob_service_client.get_blob_client(
+        container="raw", blob="pit_" + pit_data["key"] + ".json"
+    )
     with open(file=raw_path, mode="rb") as blob_data:
         blob_client.upload_blob(blob_data, overwrite=True)
-    
+
     # Save to Cosmos db
-    logging.info('Insert into Cosmos db.')
-    cosmos_client = CosmosClient(os.environ["COSMOS_URI"], credential=os.environ["COSMOS_KEY"])
+    logging.info("Insert into Cosmos db.")
+    cosmos_client = CosmosClient(
+        os.environ["COSMOS_URI"], credential=os.environ["COSMOS_KEY"]
+    )
     cosmos_database = cosmos_client.get_database_client(database=container_name)
     container = cosmos_database.get_container_client("pit")
     # Add an id to the dictionary
