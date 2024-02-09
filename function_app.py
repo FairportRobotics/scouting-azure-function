084dea8cbc89f903238abe34d641844f33605a3e
mikeasilva <mike.a.silva@gmail.com>
2024-02-08 20:04:25 -0500

    Reset bugfix take 2

--- function_app.py
+++ function_app.py
@@ -64,14 +64,15 @@ def v1(req: func.HttpRequest) -> func.HttpResponse:
         )
 
     if data_type in ["match", "pit"]:
-        if reset is None:
-            if not data:
-                # Return a different "helpful" message
-                return func.HttpResponse(
-                    json.dumps({"message": "Bummer!  No data sent to this endpoint."}),
-                    mimetype="application/json",
-                    status_code=200,
-                )
+        if reset is not None:
+            data = {"reset": reset}
+        elif not data:
+            # Return a different "helpful" message
+            return func.HttpResponse(
+                json.dumps({"message": "Bummer!  No data sent to this endpoint."}),
+                mimetype="application/json",
+                status_code=200,
+            )
     else:
         # Hmmm.  Don't know what type of data was sent
         return_data = {"message": "Error: Unknown data type sent to this endpoint!"}
